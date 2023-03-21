fd3d1e7960858221b39c880cf568984c7bf83923
Rohit Jain <rj03012002@gmail.com>
2023-03-21 17:52:29 +0530

    minor changes

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,11 +5,11 @@ import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
 export default function Layout({ children }) {
   return (
-    <Box className={styles.container} sx={{ width: "100vw" }}>
+    <Box className={styles.container}>
       <Box xs={2} className={styles.sidebar}>
         <Sidebar />
       </Box>
-      <Box xs={10} sx={{ p: 4 }} className={styles.main}>
+      <Box xs={10} className={styles.main}>
         <Header />
         {children}
       </Box>
